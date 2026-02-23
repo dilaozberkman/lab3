a256dc839f77b0edaf511700ebd38f0671cf3f58
Dila Ozberkman <dilao003@mit.edu>
2026-02-23 18:53:16 -0500

    styling reading list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,23 +25,27 @@
     I am a (super) senior at MIT double majoring in Architecture and Urban Planning. I am from Cyprus!
 </p>
 
-<img
-    class="fit-picture"
+<div class="container">
+  <section class="profile">
+    <h1>Dila Ozberkman</h1>
+    <p>I am a (super) senior at MIT double majoring in Architecture and Urban Planning. I am from Cyprus!</p>
+    <img class="fit-picture"
     width="300"
     height=auto
     src="images/dila_pic.jpg"
     alt="Dila outside MIT Building 8 on a sunny day"
-/>
+    />
+  </section>
 
-<section>
-  <h2>What I'm Reading!</h2>
-  
-  <div class="reading-list">
-    {#each reading as item}
-      <ReadingItem data={item} />
-    {/each}
-  </div>
-</section>
+  <aside class="reading-sidebar">
+    <h2>What I'm Reading</h2>
+    <div class="reading-list">
+      {#each reading as item}
+        <ReadingItem data={item} />
+      {/each}
+    </div>
+  </aside>
+</div>
 
 <h2>Latest Projects</h2>
 
@@ -51,6 +55,37 @@
 </div>
 
 <style>
+    .container {
+    display: flex;
+    gap: 4rem;
+    align-items: start;
+    margin-bottom: 3rem;
+  }
+
+  .profile {
+    flex: 2;
+  }
+
+  .reading-sidebar {
+    flex: 1;
+    background-color: oklch(from var(--border-gray) 95% 5% h); 
+    padding: 1.5rem;
+    border-radius: 12px;
+  }
+
+  .reading-sidebar h2 {
+    margin-top: 0;
+    font-size: 1.5rem;
+  }
+  
+  .reading-list {
+    display: flex;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    flex-direction: column;
+    gap: 1.5rem;
+    margin-top: 1rem;
+  }
+
   .projects {
     display: grid;
     grid-template-columns: repeat(3, 1fr); 
@@ -59,12 +94,5 @@
   }
   .projects :global(h2) {
     font-size: 150%; 
-  }
-
-  .reading-list {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-    gap: 1.5rem;
-    margin-top: 1rem;
   }
 </style>
